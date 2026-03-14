d1b24b72b795d603dd3aa53a3f7e326393310cad
juancabernal <juancamilobernal2222@gmail.com>
2026-03-14 08:42:37 -0500

    Agrega modulo proveedor_mio

---
+++ src/main/java/com/proveedor/proveedor_mio/ProveedorMioApplication.java
@@ -0,0 +1,13 @@
+package com.proveedor.proveedor_mio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveedorMioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedorMioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proveedor/proveedor_mio/ProveedorMioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proveedor.proveedor_mio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedorMioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
