fb9363ed7bdedad1e2df493c4cf1d26fa836ec46
juancabernal <juancamilobernal2222@gmail.com>
2026-03-21 09:48:58 -0500

    Se agregó todo el flujo de tax-regimes

---
+++ src/main/java/com/co/eatupapi/dto/commercial/taxRegime/TaxRegimeDTO.java
@@ -0,0 +1,37 @@
+package com.co.eatupapi.dto.commercial.taxRegime;
+
+import com.co.eatupapi.domain.commercial.taxRegime.TaxRegimeStatus;
+
+public class TaxRegimeDTO {
+
+    private String id;
+    private String name;
+    private TaxRegimeStatus status;
+
+    public TaxRegimeDTO() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TaxRegimeStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaxRegimeStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/co/eatupapi/services/commercial/taxRegime/TaxRegimeService.java
@@ -0,0 +1,19 @@
+package com.co.eatupapi.services.commercial.taxRegime;
+import com.co.eatupapi.dto.commercial.taxRegime.TaxRegimeDTO;
+
+import java.util.List;
+
+public interface TaxRegimeService {
+
+    TaxRegimeDTO createTaxRegime(TaxRegimeDTO request);
+
+    TaxRegimeDTO getTaxRegimeById(String taxRegimeId);
+
+    List<TaxRegimeDTO> getTaxRegimes(String status);
+
+    TaxRegimeDTO updateTaxRegime(String taxRegimeId, TaxRegimeDTO request);
+
+    TaxRegimeDTO changeStatus(String taxRegimeId, String status);
+}
+
+
