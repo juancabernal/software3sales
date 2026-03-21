ad979c618279145829cd930791a5c051fa42e12f
Jean Paul <jeanpaulortizrestrepo1@gmail.com>
2026-03-21 10:12:07 -0500

    Se deja el EatupapiApplication como estaba

--- src/main/java/com/co/eatupapi/EatupapiApplication.java
+++ src/main/java/com/co/eatupapi/EatupapiApplication.java
@@ -2,17 +2,8 @@ package com.co.eatupapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-
-//Ignora la libreria temporalmente de jpa y postgres, para evitar error en la compitalacion
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication()
 
 public class EatupapiApplication {
 
