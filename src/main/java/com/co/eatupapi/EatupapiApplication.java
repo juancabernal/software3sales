fcd2992752ece832a0f1585da86adef8d407dd2d
Jean Paul <jeanpaulortizrestrepo1@gmail.com>
2026-03-21 10:15:40 -0500

    Se deja  bien el EatupapiApplication

--- src/main/java/com/co/eatupapi/EatupapiApplication.java
+++ src/main/java/com/co/eatupapi/EatupapiApplication.java
@@ -3,8 +3,7 @@ package com.co.eatupapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
-
+@SpringBootApplication
 public class EatupapiApplication {
 
 	public static void main(String[] args) {
