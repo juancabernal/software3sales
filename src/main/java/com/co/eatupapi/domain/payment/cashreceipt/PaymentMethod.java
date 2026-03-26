1744ddf009ca51854df9bcee12d7a625b6b66ffb
Jean Paul <jeanpaulortizrestrepo1@gmail.com>
2026-03-26 04:42:49 -0500

    Se completa la funcionalidad metodos de pago

--- src/main/java/com/co/eatupapi/domain/payment/cashreceipt/PaymentMethod.java
+++
@@ -1,9 +0,0 @@
-package com.co.eatupapi.domain.payment.cashreceipt;
-
-public enum PaymentMethod {
-
-    CASH,
-    TRANSFER,
-    CARD
-
-}
--- src/main/java/com/co/eatupapi/utils/payment/paymentmethod/mapper/PaymentMethodMapper.java
+++ src/main/java/com/co/eatupapi/utils/payment/paymentmethod/mapper/PaymentMethodMapper.java
@@ -1,4 +1,11 @@
 package com.co.eatupapi.utils.payment.paymentmethod.mapper;
 
-public class PaymentMethodMapper {
+import com.co.eatupapi.domain.payment.paymentmethod.PaymentMethod;
+import com.co.eatupapi.dto.payment.paymentmethod.PaymentMethodResponse;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMethodMapper {
+
+    PaymentMethodResponse toResponse(PaymentMethod paymentMethod);
 }
