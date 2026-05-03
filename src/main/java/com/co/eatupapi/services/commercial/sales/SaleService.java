d3a93e8ef596add50db9c5cd8baea0224de2853d
Juan Camilo Bernal <juancamilobernal2222@gmail.com>
2026-05-02 20:54:23 -0500

    Move sales RabbitMQ properties to commercial config

--- src/main/java/com/co/eatupapi/services/commercial/sales/SaleService.java
+++ src/main/java/com/co/eatupapi/services/commercial/sales/SaleService.java
@@ -1,18 +1,16 @@
 package com.co.eatupapi.services.commercial.sales;
 
 import com.co.eatupapi.dto.commercial.sales.SalePatchDTO;
-import com.co.eatupapi.dto.commercial.sales.SaleAsyncResponseDTO;
 import com.co.eatupapi.dto.commercial.sales.SaleRequestDTO;
 import com.co.eatupapi.dto.commercial.sales.SaleResponseDTO;
-
 import java.util.List;
 import java.util.UUID;
 
 public interface SaleService {
-    SaleAsyncResponseDTO createSale(SaleRequestDTO request);
+    SaleResponseDTO createSale(SaleRequestDTO request);
     SaleResponseDTO getSaleById(UUID id);
     List<SaleResponseDTO> getAllSales();
-    SaleAsyncResponseDTO updateSale(UUID id, SaleRequestDTO request);
-    SaleAsyncResponseDTO patchSale(UUID id, SalePatchDTO request);
-    SaleAsyncResponseDTO deleteSale(UUID id);
+    SaleResponseDTO updateSale(UUID id, SaleRequestDTO request);
+    SaleResponseDTO patchSale(UUID id, SalePatchDTO request);
+    void deleteSale(UUID id);
 }
